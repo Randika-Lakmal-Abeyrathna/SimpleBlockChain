ec47a7e1560fd5cba2b19329b77226cacfbb2a3f
Randika Abeyrathna <randika.l.abeyrathna@gmail.com>
2022-02-02 12:48:05 +0530

    Create Block Class

---
+++ src/Block.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Block {
+
+    private int previousHash;
+    private String[] transaction;
+    private int blockHash;
+
+    public Block(int previousHash, String[] transaction) {
+        this.previousHash = previousHash;
+        this.transaction = transaction;
+
+        Object[] contens = {Arrays.hashCode(transaction),previousHash};
+        this.blockHash= Arrays.hashCode(contens);
+    }
+
+    public int getPreviousHash() {
+        return previousHash;
+    }
+
+    public String[] getTransaction() {
+        return transaction;
+    }
+
+}
